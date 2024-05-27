8a0a8a84c221c22b69384942a4c01244f9e88f76
SuperMarianWorld <cyberwargamesproductions@gmail.com>
2024-05-27 01:12:03 -0300

    Carga del proyecto base con algunos avances hechos

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function dataTime() {
+    document.getElementById(#dataTime);
+    dataTime = new Date();
+    update=dataTime.getFullYear();
+}
+
+document.write("© Copyright | Instituto Superior de Formación Técnica N°12" + update);
